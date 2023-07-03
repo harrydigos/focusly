4dea14b4b5204c62d242af93ba61992b565ccc95
harrydigos <harrydigos@gmail.com>
2023-07-03 23:32:24 +0300

    Remove boilerplate + allow only 1 route

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "solid-start";
 import {
   StartServer,
   createHandler,
@@ -5,5 +6,18 @@ import {
 } from "solid-start/entry-server";
 
 export default createHandler(
+  ({ forward }) => {
+    return async (event) => {
+      const pathname = new URL(event.request.url).pathname;
+
+      /* Redirect all routes to "/" */
+
+      if (pathname === "/") {
+        return forward(event);
+      }
+
+      return redirect("/");
+    };
+  },
   renderAsync((event) => <StartServer event={event} />)
 );
