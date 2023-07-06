22df5b24c62b315d064b19fe6a0e4b8a62383879
harrydigos <harrydigos@gmail.com>
2023-07-07 00:52:28 +0300

    Toggling menu tabs

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,28 +1,38 @@
-import { Component } from "solid-js";
+import { Component, For } from "solid-js";
 import { GlassBox } from "~/design/GlassBox";
-import { TbListCheck, TbMusic, TbNote } from "solid-icons/tb";
-import { IconTypes } from "solid-icons";
+import {
+  MenuTabs,
+  MenuTabsStore,
+  menuTabsStore,
+  toggleTab,
+} from "~/stores/MenuTabsStore";
 
 export const Menu: Component = () => {
+  const tabs = menuTabsStore;
+
   return (
     <header class="w-full flex justify-center mt-4">
       <GlassBox direction="flex-row" class="w-fit p-1 gap-1">
-        <MenuItem icon={TbListCheck} label="Todos" />
-        <MenuItem icon={TbMusic} label="Music" />
-        <MenuItem icon={TbNote} label="Notes" />
+        <For each={Object.entries(tabs)}>
+          {([key, value]) => <MenuItem key={key as MenuTabs} {...value} />}
+        </For>
       </GlassBox>
     </header>
   );
 };
 
-const MenuItem: Component<{ icon: IconTypes; label: string }> = (props) => {
+const MenuItem: Component<{ key: MenuTabs } & MenuTabsStore> = (props) => {
   return (
     <button
       type="button"
       class="flex gap-1 p-2 hover:bg-stone-900 hover:bg-opacity-30 transition-colors rounded-full"
+      classList={{
+        "bg-stone-100 bg-opacity-30": props.isOpen,
+      }}
+      onClick={() => toggleTab(props.key)}
     >
       <props.icon size={24} class="text-stone-100" />
-      <div>{props.label}</div>
+      <div class="capitalize">{props.label}</div>
     </button>
   );
 };
