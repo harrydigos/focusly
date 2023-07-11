132321812084d3410459f88b3c78d1c50a3b35a0
harrydigos <harrydigos@gmail.com>
2023-07-11 11:32:57 +0300

    Todos form

--- src/types/menu.ts
+++ src/types/menu.ts
@@ -1,8 +1,10 @@
-import { initialMenu } from "~/config";
+import { XOR } from "~/types";
 
-export type MenuKey = keyof typeof initialMenu;
+export type MenuKey = "todos" | "music" | "notes";
 
-export interface MenuTab {
+export type MenuTab = Tab & XOR<XOR<Todos, Notes>, Music>;
+
+export interface Tab {
   isOpen: boolean;
   position: Position;
 }
@@ -11,4 +13,27 @@ export interface Position {
   x: number;
   y: number;
   z: number;
+}
+
+export interface Todos {
+  todosList: Todo[];
+}
+
+export interface Todo {
+  id: string;
+  value: string;
+  completed: boolean;
+}
+
+export interface Music {
+  song: string;
+}
+
+export interface Notes {
+  notes: string[];
+}
+
+export interface Note {
+  id: string;
+  value: string;
 }
