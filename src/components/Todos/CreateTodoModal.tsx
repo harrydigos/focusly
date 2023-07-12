0e3d66dbf8427addd4c97827ee2c220c4421d21a
harrydigos <harrydigos@gmail.com>
2023-07-12 18:42:08 +0300

    Improve todos edit, delete, create

---
+++ src/components/Todos/CreateTodoModal.tsx
@@ -0,0 +1,77 @@
+import {
+  SubmitHandler,
+  createForm,
+  maxLength,
+  required,
+  reset,
+} from "@modular-forms/solid";
+import { Component } from "solid-js";
+
+import { Modal } from "~/design/Modal";
+import { Stack } from "~/design/Stack";
+import { setMenuTabs } from "~/stores/MenuTabsStore";
+import { Todo } from "~/types";
+
+type TodoForm = {
+  todo: string;
+};
+
+export const CreateTodoModal: Component = () => {
+  const [form, { Form, Field }] = createForm<TodoForm>({
+    initialValues: {
+      todo: "",
+    },
+  });
+
+  const handleSubmit: SubmitHandler<TodoForm> = (values) => {
+    const newTodo: Todo = {
+      id: Date.now().toString(),
+      value: values.todo,
+      completed: false,
+    };
+
+    setMenuTabs("todos", "todosList", (prev) =>
+      prev ? [...prev, newTodo] : [newTodo]
+    );
+    reset(form);
+  };
+
+  return (
+    <Modal
+      trigger={() => (
+        <button class="bg-white text-stone-900 rounded-lg inline-flex items-center h-10 px-3">
+          New
+        </button>
+      )}
+    >
+      <Form onSubmit={handleSubmit}>
+        <Field
+          name={"todo"}
+          validate={[
+            required("Please type a todo."),
+            maxLength(150, "Too long todo."),
+          ]}
+        >
+          {(field, props) => (
+            <Stack direction="flex-col">
+              <Stack direction="flex-row">
+                <input
+                  spellcheck={false}
+                  placeholder="Create todo"
+                  autocomplete="off"
+                  type="text"
+                  value={field.value}
+                  class="text-stone-900"
+                  required
+                  {...props}
+                />
+                <button type="submit">Add</button>
+              </Stack>
+              {field.error && <div>{field.error}</div>}
+            </Stack>
+          )}
+        </Field>
+      </Form>
+    </Modal>
+  );
+};
--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,84 +1,27 @@
+import { TbEdit, TbEditOff, TbTrash } from "solid-icons/tb";
 import {
-  SubmitHandler,
-  createForm,
-  maxLength,
-  required,
-  reset,
-} from "@modular-forms/solid";
-import { Component, For, Show, createMemo, createSignal } from "solid-js";
-import { Modal } from "~/design/Modal";
+  Component,
+  For,
+  Show,
+  createEffect,
+  createMemo,
+  createSignal,
+} from "solid-js";
 import { Stack } from "~/design/Stack";
 import { menuTabs, setMenuTabs } from "~/stores/MenuTabsStore";
 import { Todo } from "~/types";
-
-type TodoForm = {
-  todo: string;
-};
+import { CreateTodoModal } from "./CreateTodoModal";
 
 export const Todos: Component = () => {
-  const [form, { Form, Field }] = createForm<TodoForm>({
-    initialValues: {
-      todo: "",
-    },
-  });
-
-  const handleSubmit: SubmitHandler<TodoForm> = (values) => {
-    const newTodo: Todo = {
-      id: Date.now().toString(),
-      value: values.todo,
-      completed: false,
-    };
-
-    setMenuTabs("todos", "todosList", (prev) =>
-      prev ? [...prev, newTodo] : [newTodo]
-    );
-    reset(form);
-  };
-
   return (
-    <Stack
-      direction="flex-col"
-      class="max-w-[300px] w-full overflow-hidden gap-2"
-    >
-      <h1 class="text-lg font-semibold">Todos</h1>
-      <Form onSubmit={handleSubmit}>
-        <Field
-          name={"todo"}
-          validate={[
-            required("Please type a todo."),
-            maxLength(150, "Too long todo."),
-          ]}
-        >
-          {(field, props) => (
-            <Stack direction="flex-col">
-              <Stack direction="flex-row">
-                <input
-                  spellcheck={false}
-                  placeholder="Create todo"
-                  autocomplete="off"
-                  type="text"
-                  value={field.value}
-                  class="text-stone-900"
-                  required
-                  {...props}
-                />
-                <button type="submit">Add</button>
-              </Stack>
-              {field.error && <div>{field.error}</div>}
-            </Stack>
-          )}
-        </Field>
-      </Form>
+    <Stack direction="flex-col" class="w-72 overflow-hidden gap-2">
+      <Stack direction="flex-row" class="justify-between">
+        <h1 class="text-lg font-semibold">Todos</h1>
+        <CreateTodoModal />
+      </Stack>
       <For each={menuTabs.todos.todosList}>
         {(todo) => <TodoRow {...todo} />}
       </For>
-      <Modal
-        trigger={() => (
-          <button onClick={() => console.log("hehe")}>Click</button>
-        )}
-      >
-        <div>Content</div>
-      </Modal>
     </Stack>
   );
 };
@@ -103,42 +46,69 @@ const TodoRow: Component<Todo> = (todo) => {
     );
   };
 
+  let el: HTMLInputElement | undefined;
+
+  createEffect(() => {
+    if (isEditing()) {
+      el?.focus();
+    }
+  });
+
   return (
     <Stack direction="flex-row" class="justify-between">
-      <button onClick={() => completeTodo()}>
-        {todo.completed ? "Undone" : "Done"}
-      </button>
-      <Show when={!isEditing()}>
-        <span
-          class="w-64"
-          onDblClick={() => setIsEditing(true)}
-          classList={{
-            "line-through opacity-50": todo.completed,
-          }}
-        >
-          {todo.value}
-        </span>
-      </Show>
-      <Show when={isEditing()}>
-        {/* Handle constraints when editing */}
+      <Stack direction="flex-row" class="gap-1">
         <input
-          class="bg-stone-900 w-64"
-          autofocus={true}
-          onBlur={() => setIsEditing(false)}
-          value={todo.value}
-          onInput={(e) => {
-            setMenuTabs("todos", "todosList", todoIndex(), (prev) => {
-              return { ...prev, value: e.target.value };
-            });
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" || e.key === "Escape") {
-              setIsEditing(false);
-            }
-          }}
+          id={todo.id}
+          type="checkbox"
+          checked={todo.completed}
+          onChange={completeTodo}
         />
-      </Show>
-      <button onClick={() => deleteTodo()}>Delete</button>
+        <div class="w-52">
+          <Show when={!isEditing()}>
+            <label
+              for={todo.id}
+              class="w-full break-words cursor-pointer"
+              classList={{
+                "line-through opacity-50": todo.completed,
+              }}
+            >
+              {todo.value}
+            </label>
+          </Show>
+          <Show when={isEditing()}>
+            {/* Handle constraints when editing */}
+            <input
+              ref={el}
+              class="w-full bg-stone-900 break-words"
+              classList={{
+                "line-through": todo.completed,
+              }}
+              onBlur={() => setIsEditing(false)}
+              autofocus
+              value={todo.value}
+              onInput={(e) => {
+                setMenuTabs("todos", "todosList", todoIndex(), (prev) => ({
+                  ...prev,
+                  value: e.target.value,
+                }));
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" || e.key === "Escape") {
+                  setIsEditing(false);
+                }
+              }}
+            />
+          </Show>
+        </div>
+      </Stack>
+      <button onClick={() => setIsEditing(true)}>
+        <Show when={!isEditing()} fallback={<TbEditOff size={20} />}>
+          <TbEdit size={20} />
+        </Show>
+      </button>
+      <button onClick={deleteTodo}>
+        <TbTrash size={20} />
+      </button>
     </Stack>
   );
 };
