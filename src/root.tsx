341408f5625f50ea1cb7c50d1f3f0600e04948dd
harrydigos <harrydigos@gmail.com>
2023-07-15 22:29:57 +0300

    feat: Deploy

--- src/root.tsx
+++ src/root.tsx
@@ -29,12 +29,12 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>4Focus</Title>
+        <Title>Focusly</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <Body class="bg-black text-white font-lexend">
+      <Body class="bg-black font-lexend text-white">
         <Suspense>
           <ErrorBoundary>
             <Routes>
