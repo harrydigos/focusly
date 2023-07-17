1806cc9baba887c1c586e643df7573729efe43d3
harrydigos <harrydigos@gmail.com>
2023-07-17 15:32:51 +0300

    feat: Create, delete notes

--- src/config/menu.ts
+++ src/config/menu.ts
@@ -27,18 +27,8 @@ export const initialNotePanel: Tab = {
 
 export const initialNotes: Note[] = [
   {
-    id: "1689528313861",
-    value: "Note 1",
-    position: {
-      x: 0,
-      y: 0,
-      z: 0,
-    },
-    isOpen: false,
-  },
-  {
     id: Date.now().toString(),
-    value: "Note 2",
+    value: "Example note",
     position: {
       x: 0,
       y: 0,
