0c53d4ce7b109258822f2553306bbeb5e20ab203
harrydigos <harrydigos@gmail.com>
2023-07-26 14:50:15 +0300

    feat: Note reordering
    Similar to todo ordering

--- src/components/NoteControl/index.tsx
+++ src/components/NoteControl/index.tsx
@@ -1,5 +1,27 @@
-import { Accessor, Component, For, Show, createMemo } from "solid-js";
-import { TbEye, TbEyeOff, TbPlus, TbTrash } from "solid-icons/tb";
+import {
+  Accessor,
+  Component,
+  For,
+  Show,
+  createMemo,
+  createSignal,
+} from "solid-js";
+import {
+  TbEye,
+  TbEyeOff,
+  TbGripVertical,
+  TbPlus,
+  TbTrash,
+} from "solid-icons/tb";
+import {
+  closestCenter,
+  createSortable,
+  DragDropProvider,
+  DragDropSensors,
+  DragOverlay,
+  SortableProvider,
+  useDragDropContext,
+} from "@thisbeyond/solid-dnd";
 
 import { Draggable } from "~/components/Draggable";
 import { Button } from "~/design/Button";
@@ -11,6 +33,26 @@ import { usePanelContext } from "~/providers";
 export const NoteControl: Component = () => {
   const { noteControl, setNoteControl, notes, setNotes, getBiggestZ } =
     usePanelContext();
+
+  const [reorder, setReorder] = createSignal(false);
+  const ids = () => notes.map((t) => t.id);
+
+  const onDragEnd = (obj: any) => {
+    if (obj.draggable && obj.droppable) {
+      const fromIndex = ids().indexOf(obj.draggable.id);
+      const toIndex = ids().indexOf(obj.droppable.id);
+
+      if (fromIndex !== toIndex) {
+        const updatedItems = ids().slice();
+        updatedItems.splice(toIndex, 0, ...updatedItems.splice(fromIndex, 1));
+        setNotes(() =>
+          updatedItems
+            .map((id) => notes.find((t) => t.id === id)!)
+            .filter((t) => t !== null)
+        );
+      }
+    }
+  };
 
   const createNote = () => {
     const newNote: Note = {
@@ -28,30 +70,47 @@ export const NoteControl: Component = () => {
 
   return (
     <Show when={noteControl.isOpen}>
-      <Draggable tab={noteControl} setTab={setNoteControl}>
+      <Draggable tab={noteControl} setTab={setNoteControl} disabled={reorder()}>
         <GlassBox
           direction="flex-col"
-          class="max-h-[500px] w-[340px] gap-4 sm:w-[440px]"
+          class="max-h-[500px] w-[340px] gap-4 px-0 sm:w-[440px]"
         >
-          <Stack direction="flex-row" class="items-center justify-between">
+          <Stack direction="flex-row" class="items-center justify-between px-6">
             <h1 class="text-xl font-semibold">Notes</h1>
             <Button onClick={createNote}>
               <TbPlus size={20} class="stroke-stone-900" />
               New note
             </Button>
           </Stack>
-          <Stack direction="flex-col" class="gap-1 overflow-y-auto py-1">
-            <For
-              each={notes}
-              fallback={
-                <span class="text-center text-sm text-stone-200">
-                  No notes yet. Add one by clicking the button above.
-                </span>
-              }
+          <DragDropProvider
+            onDragEnd={onDragEnd}
+            collisionDetector={closestCenter}
+          >
+            <DragDropSensors />
+            <Stack
+              direction="flex-col"
+              class="overflow-y-auto py-1"
+              onMouseEnter={() => setReorder(true)}
+              onMouseLeave={() => setReorder(false)}
             >
-              {(note, i) => <NoteRow note={note} index={i} />}
-            </For>
-          </Stack>
+              <SortableProvider ids={ids()}>
+                <For
+                  each={notes}
+                  fallback={
+                    <span class="text-center text-sm text-stone-200">
+                      No notes yet. Add one by clicking the button above.
+                    </span>
+                  }
+                >
+                  {(note, i) => <NoteRow note={note} index={i} />}
+                </For>
+              </SortableProvider>
+            </Stack>
+            {/* This is necessary */}
+            <DragOverlay>
+              <div class="hidden" />
+            </DragOverlay>
+          </DragDropProvider>
         </GlassBox>
       </Draggable>
     </Show>
@@ -59,6 +118,9 @@ export const NoteControl: Component = () => {
 };
 
 const NoteRow: Component<{ note: Note; index: Accessor<number> }> = (props) => {
+  // eslint-disable-next-line solid/reactivity
+  const sortable = createSortable(props.note.id);
+  const ctx = useDragDropContext();
   const { notes, setNotes, getBiggestZ } = usePanelContext();
 
   const toggleNote = () => {
@@ -99,43 +161,66 @@ const NoteRow: Component<{ note: Note; index: Accessor<number> }> = (props) => {
   });
 
   return (
-    <Stack
-      direction="flex-row"
-      class="cursor-pointer items-center justify-between gap-1 rounded-lg border border-transparent bg-stone-900/50 p-2 text-white hover:bg-stone-800/50"
-      onClick={toggleNote}
+    <div
+      use:sortable
+      class="group relative px-6 py-0.5"
+      classList={{
+        "opacity-30": sortable.isActiveDraggable,
+        "transition-transform cursor-grabbing": !!ctx?.[0].active.draggable,
+        "cursor-grab": !ctx?.[0].active.draggable,
+      }}
     >
-      <Stack direction="flex-col">
-        <Stack direction="flex-row" class="items-center gap-2">
-          <h2 class="max-w-[205px] truncate text-sm sm:max-w-[300px]">
-            {noteValue().title}
-          </h2>
-          <Show
-            when={props.note.isOpen}
-            fallback={<TbEyeOff class="opacity-50" />}
-          >
-            <TbEye class="opacity-50" />
-          </Show>
-        </Stack>
-        <Stack
-          direction="flex-row"
-          class=" gap-2 text-xs font-light text-stone-300"
-        >
-          <span>{updatedAt()}</span>
-          <div class="max-w-[140px] truncate sm:max-w-[240px]">
-            {noteValue().text}
-          </div>
-        </Stack>
-      </Stack>
-      <Button
-        variant="ghost"
-        size="icon"
-        onClick={(e: MouseEvent) => {
-          e.stopPropagation();
-          deleteNote();
-        }}
+      {/* Both Shows are needed, cuz to reorder, you need to place the cursor above another TodoRow */}
+      <Show when={ctx?.[0].active.draggable?.id === props.note.id}>
+        <TbGripVertical
+          size={16}
+          class="absolute left-1 top-1/2 -translate-y-1/2 stroke-white/80"
+        />
+      </Show>
+      <Show when={!ctx?.[0].active.draggable}>
+        <TbGripVertical
+          size={16}
+          class="absolute left-1 top-1/2 -translate-y-1/2 stroke-white/40 opacity-0 group-hover:opacity-100"
+        />
+      </Show>
+      <Stack
+        direction="flex-row"
+        class="cursor-pointer select-none items-center justify-between gap-1 rounded-lg border border-transparent bg-stone-900/50 p-2 text-white hover:bg-stone-800/50"
+        onClick={toggleNote}
       >
-        <TbTrash size={20} class="stroke-red-500" />
-      </Button>
-    </Stack>
+        <Stack direction="flex-col">
+          <Stack direction="flex-row" class="items-center gap-2">
+            <h2 class="max-w-[205px] truncate text-sm sm:max-w-[300px]">
+              {noteValue().title}
+            </h2>
+            <Show
+              when={props.note.isOpen}
+              fallback={<TbEyeOff class="opacity-50" />}
+            >
+              <TbEye class="opacity-50" />
+            </Show>
+          </Stack>
+          <Stack
+            direction="flex-row"
+            class=" gap-2 text-xs font-light text-stone-300"
+          >
+            <span>{updatedAt()}</span>
+            <div class="max-w-[140px] truncate sm:max-w-[240px]">
+              {noteValue().text}
+            </div>
+          </Stack>
+        </Stack>
+        <Button
+          variant="ghost"
+          size="icon"
+          onClick={(e: MouseEvent) => {
+            e.stopPropagation();
+            deleteNote();
+          }}
+        >
+          <TbTrash size={20} class="stroke-red-500" />
+        </Button>
+      </Stack>
+    </div>
   );
 };
