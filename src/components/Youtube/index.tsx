936217d7cf61f5d1a6244a8470e956d857dd4612
harrydigos <harrydigos@gmail.com>
2023-08-05 22:44:29 +0300

    feat: Music volume + mute

--- src/components/Youtube/index.tsx
+++ src/components/Youtube/index.tsx
@@ -1,12 +1,33 @@
-import { Component, Match, Switch, createSignal, onMount } from "solid-js";
 import {
+  Component,
+  Match,
+  Show,
+  Switch,
+  createEffect,
+  createSignal,
+  onMount,
+} from "solid-js";
+import {
+  TbAccessPoint,
   TbLoader,
   TbPlayerPauseFilled,
   TbPlayerPlayFilled,
+  TbVolume,
+  TbVolume2,
+  TbVolume3,
 } from "solid-icons/tb";
+
 import YoutubeFactory from "youtube-player";
 import type { YouTubePlayer } from "youtube-player/dist/types";
 import PlayerStates from "youtube-player/dist/constants/PlayerStates";
+
+import {
+  Slider,
+  SliderControl,
+  SliderRange,
+  SliderThumb,
+  SliderTrack,
+} from "@ark-ui/solid";
 
 import { usePanelContext } from "~/providers";
 import { Draggable } from "~/components/Draggable";
@@ -19,6 +40,8 @@ export const YoutubePlayer: Component = () => {
 
   const [container, setContainer] = createSignal<HTMLDivElement>();
   const [player, setPlayer] = createSignal<YouTubePlayer>();
+  const [volume, setVolume] = createSignal(25);
+  const [isMuted, setIsMuted] = createSignal(false);
 
   const [playerState, setPlayerState] = createSignal<PlayerStates>(
     PlayerStates.UNSTARTED
@@ -39,10 +62,28 @@ export const YoutubePlayer: Component = () => {
 
     /* Default settings when player is initialized */
     player()?.stopVideo();
-    player()?.setVolume(25);
+    player()?.setVolume(volume());
 
     player()?.on("stateChange", ({ data }) => setPlayerState(data));
   });
+
+  createEffect(() => {
+    player()?.setVolume(volume());
+  });
+
+  const toggleMute = () => {
+    if (volume() === 0) {
+      /* When volume is 0, muted icon is displayed.
+       * Volume equal to 0 and isMuted is the same thing but handled with 2 different signals.
+       */
+      setIsMuted(false);
+      setVolume(25);
+    } else {
+      setIsMuted((prev) => !prev);
+    }
+
+    isMuted() ? player()?.mute() : player()?.unMute(); // Synchronize
+  };
 
   return (
     <Draggable tab={music} setTab={setMusic}>
@@ -57,8 +98,39 @@ export const YoutubePlayer: Component = () => {
         <Stack direction="flex-row" class="items-center justify-between">
           <h1 class="text-xl font-semibold">Music</h1>
         </Stack>
+        <Stack direction="flex-row" class="items-center justify-between gap-2">
+          <Stack direction="flex-row" class="items-center gap-2">
+            <PlayButton player={player()} state={playerState()} />
+            <Stack direction="flex-row" class="h-8 items-center gap-1">
+              <h3 class="relative text-sm font-normal">Lofi Hip Hop</h3>
+              <TbAccessPoint class="stroke-red-500" />
+            </Stack>
+          </Stack>
+          <Stack direction="flex-row" class="items-center gap-1">
+            <Button variant="outline" size="icon" onClick={toggleMute}>
+              <Show when={!isMuted() && volume() > 0} fallback={<TbVolume3 />}>
+                <Show when={volume() >= 50} fallback={<TbVolume2 />}>
+                  <TbVolume />
+                </Show>
+              </Show>
+            </Button>
+            <Slider
+              min={0}
+              max={100}
+              value={volume()}
+              onChange={({ value }) => setVolume(value)}
+              class="w-24"
+            >
+              <SliderControl class="relative flex h-6 w-full items-center">
+                <SliderTrack class="h-1 w-full rounded-full bg-stone-900">
+                  <SliderRange class="h-1 rounded-full bg-white" />
+                </SliderTrack>
+                <SliderThumb class="h-3 w-3 rounded-full bg-white" />
+              </SliderControl>
+            </Slider>
+          </Stack>
+        </Stack>
         <div ref={setContainer} class="hidden" />
-        <PlayButton player={player()} state={playerState()} />
       </GlassBox>
     </Draggable>
   );
