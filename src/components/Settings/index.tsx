ba73f06ab174f599057b7c011ced3c72c3bde3c5
harrydigos <harrydigos@gmail.com>
2023-08-29 12:46:57 +0300

    Modal UI prototype
    With Lock/Unlock, Customization preferences and reset

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,6 +1,6 @@
 import { Dialog } from "@ark-ui/solid";
 import { TbX } from "solid-icons/tb";
-import { Accessor, Component, Setter } from "solid-js";
+import { Accessor, Component, For, Setter } from "solid-js";
 import { Button } from "~/design/Button";
 import { Modal } from "~/design/Modal";
 import { Stack } from "~/design/Stack";
@@ -14,15 +14,74 @@ export const SettingsModal: Component<SettingsModalProps> = (props) => {
   return (
     <Dialog open={props.isOpen()} onClose={() => props.setIsOpen(false)}>
       <Modal isOpen={props.isOpen}>
-        <Stack direction="flex-row" class="w-full items-center justify-between">
-          <h1 class="text-lg font-semibold">Settings</h1>
-          <Button
-            variant="outline"
-            size="icon"
-            onClick={() => props.setIsOpen(false)}
+        <Stack direction="flex-col" class="gap-3">
+          <Stack
+            direction="flex-row"
+            class="w-full items-center justify-between"
           >
-            <TbX class="h-4 w-4" />
-          </Button>
+            <h1 class="text-lg font-semibold">Settings</h1>
+            <Button
+              variant="outline"
+              size="icon"
+              onClick={() => props.setIsOpen(false)}
+            >
+              <TbX class="h-4 w-4" />
+            </Button>
+          </Stack>
+          <hr class="h-px border-0 bg-stone-800" />
+          <Stack
+            direction="flex-row"
+            class="w-full items-center justify-between"
+          >
+            <h2 class="text-sm">Lock panels</h2>
+            {/* TODO: make this a switch */}
+            <Button variant="outline">Lock</Button>
+          </Stack>
+          <hr class="h-px border-0 bg-stone-800" />
+          <Stack
+            direction="flex-row"
+            class="w-full items-center justify-between gap-8 whitespace-nowrap"
+          >
+            <h2 class="text-sm">Panel color</h2>
+
+            <div class="grid grid-cols-5 gap-1">
+              <For each={Array(10).fill(0)}>
+                {() => (
+                  <div class="aspect-square h-6 rounded-md bg-stone-600" />
+                )}
+              </For>
+            </div>
+          </Stack>
+          <hr class="h-px border-0 bg-stone-800" />
+          <Stack
+            direction="flex-row"
+            class="w-full items-center justify-between"
+          >
+            <h2 class="text-sm">Space</h2>
+            <div class="h-20 w-28 rounded-xl bg-stone-600" />
+          </Stack>
+          <hr class="h-px border-0 bg-stone-800" />
+          <Stack
+            direction="flex-row"
+            class="w-full items-center justify-between"
+          >
+            <h2 class="text-sm">Alarm</h2>
+            <Stack direction="flex-row" class="gap-2">
+              <div class="aspect-square h-12 rounded-lg bg-stone-600" />
+              <div class="aspect-square h-12 rounded-lg bg-stone-600" />
+              <div class="aspect-square h-12 rounded-lg bg-stone-600" />
+            </Stack>
+          </Stack>
+          <hr class="h-px border-0 bg-stone-800" />
+          <Stack
+            direction="flex-row"
+            class="w-full items-center justify-between"
+          >
+            <h2 class="text-sm">Destructive</h2>
+            <Stack direction="flex-row" class="gap-3">
+              <Button variant="destructive">Reset</Button>
+            </Stack>
+          </Stack>
         </Stack>
       </Modal>
     </Dialog>
--- src/design/Button/index.tsx
+++ src/design/Button/index.tsx
@@ -12,7 +12,8 @@ export const Button = klassed(
         default: ["bg-stone-50 text-stone-900", "hover:bg-stone-50/90"],
         secondary: ["bg-stone-900 text-stone-50", "hover:bg-stone-900/90"],
         outline: ["border border-stone-600 bg-transparent", "hover:opacity-90"],
-        ghost: "hover:bg-stone-800",
+        ghost: ["hover:bg-stone-800"],
+        destructive: ["bg-red-700 text-stone-50", "hover:bg-red-700/90"],
       },
       size: {
         default: "h-10 px-4 py-2 gap-1",
