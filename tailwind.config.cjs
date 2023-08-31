16b3736d0aaeb7081e1145618425277de9ebe80a
harrydigos <harrydigos@gmail.com>
2023-08-31 13:24:12 +0300

    feat: Background color select

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         lexend: ["var(--primary-font)"],
       },
+      colors: {
+        bgColor: "var(--bg-color)",
+      },
     },
   },
   plugins: [
