1bb6a27b9384259663c4bf0fb9e17d80c9563b96
Harry Digos <harrydigos@gmail.com>
2024-01-14 19:09:16 +0200

    fix: home page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,77 @@
-import { Title } from "@solidjs/meta";
+import { TbPlanet, TbSettings } from "solid-icons/tb";
+import { Suspense, createSignal, lazy, Show, onMount } from "solid-js";
+import { Background } from "~/components/Background";
+import { YoutubeProvider } from "~/providers";
+import { Button } from "~/design/Button";
+import { Stack } from "~/design/Stack";
+import { PanelProvider } from "~/providers";
+
+const Menu = lazy(() =>
+  import("~/components/Menu").then((m) => ({ default: m.Menu }))
+);
+
+const Panels = lazy(() =>
+  import("~/components/Panels").then((m) => ({ default: m.Panels }))
+);
+
+const SettingsModal = lazy(() =>
+  import("~/components/Settings").then((m) => ({ default: m.SettingsModal }))
+);
+
+const SpacesModal = lazy(() =>
+  import("~/components/Spaces").then((m) => ({ default: m.SpacesModal }))
+);
 
 export default function Home() {
+  const [isMounted, setIsMounted] = createSignal(false);
+  const [openSettings, setOpenSettings] = createSignal(false);
+  const [openSpaces, setOpenSpaces] = createSignal(false);
+
+  onMount(() => setIsMounted(true));
+
   return (
-    <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+    <main class="screen">
+      <Background />
+      <Show when={isMounted()}>
+        <YoutubeProvider>
+          <PanelProvider>
+            <Suspense>
+              <Panels />
+              <Menu />
+              <SettingsModal
+                isOpen={openSettings}
+                setIsOpen={setOpenSettings}
+              />
+              <SpacesModal isOpen={openSpaces} setIsOpen={setOpenSpaces} />
+            </Suspense>
+          </PanelProvider>
+        </YoutubeProvider>
+      </Show>
+      <Stack direction="flex-col" class="absolute bottom-4 left-4 gap-2">
+        <Button
+          variant="secondary"
+          class="h-10 w-10"
+          onClick={() => setOpenSpaces(true)}
+        >
+          <TbPlanet class="h-5 w-5" />
+        </Button>
+        <Button
+          variant="secondary"
+          class="h-10 w-10"
+          onClick={() => setOpenSettings(true)}
+        >
+          <TbSettings class="h-5 w-5" />
+        </Button>
+      </Stack>
+      <p class="absolute bottom-0 right-0 w-fit select-none rounded-tl-xl bg-stone-900 px-2 py-1.5 text-xs font-medium leading-none text-white">
+        <span class="text-[10px]">by </span>
+        <a
+          href="https://twitter.com/harry_digos"
+          target="blank"
+          class="hover:underline"
+        >
+          @harry_digos
+        </a>
       </p>
     </main>
   );
