7cef56c313b9ae5d93ad43ec6bafd05f1470e48c
Harry Digos <harrydigos@gmail.com>
2024-01-24 22:41:02 +0200

    feat: calculate animation duration based on distance
    + enter/exit easings

--- src/components/AnimatePanel/index.tsx
+++ src/components/AnimatePanel/index.tsx
@@ -32,28 +32,50 @@ export const AnimatePanel: Component<AnimatePanelProps> = (props) => {
   });
 
   const getAnimations = ({ width, height }: DOMRect) => {
-    return [
-      {
-        transform: `translate3d(${position().from.x - width / 2}px, ${position().from.y - height / 2
-          }px, 0px) scale(0)`,
-      },
-      {
-        transform: `translate3d(${position().to.x}px, ${position().to.y
-          }px, 0px) scale(1)`,
-      },
-    ];
+    const centerFrom = {
+      x: (position().from.x + width) / 2,
+      y: (position().from.y + height) / 2,
+    };
+
+    const centerTo = {
+      x: (position().to.x + width) / 2,
+      y: (position().to.y + height) / 2,
+    };
+
+    const duration = Math.sqrt(
+      Math.pow(centerFrom.x - centerTo.x, 2) +
+      Math.pow(centerFrom.y - centerTo.y, 2)
+    );
+
+    return {
+      animation: [
+        {
+          transform: `translate3d(${position().from.x - width / 2}px, ${position().from.y - height / 2
+            }px, 0px) scale(0)`,
+        },
+        {
+          transform: `translate3d(${position().to.x}px, ${position().to.y
+            }px, 0px) scale(1)`,
+        },
+      ],
+      duration: Math.min(duration * 0.5 + 150, 500),
+    };
   };
 
   const onEnter = (el: Element, done: () => void) => {
-    const a = el.animate(getAnimations(el.getBoundingClientRect()), {
-      duration: 200,
+    const { animation, duration } = getAnimations(el.getBoundingClientRect());
+    const a = el.animate(animation, {
+      duration,
+      easing: "cubic-bezier(0.250, 0.460, 0.450, 0.940)",
     });
     a.finished.then(done);
   };
 
   const onExit = (el: Element, done: () => void) => {
-    const a = el.animate(getAnimations(el.getBoundingClientRect()).reverse(), {
-      duration: 200,
+    const { animation, duration } = getAnimations(el.getBoundingClientRect());
+    const a = el.animate(animation.reverse(), {
+      duration,
+      easing: "cubic-bezier(0.550, 0.085, 0.680, 0.530)",
     });
     a.finished.then(done);
   };
