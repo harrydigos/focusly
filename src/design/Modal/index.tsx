360d336ce66c3de8cd99712904d0d74d93da17b5
Harry Digos <harrydigos@gmail.com>
2024-02-10 11:05:27 +0200

    feat: mount modal dynamically

--- src/design/Modal/index.tsx
+++ src/design/Modal/index.tsx
@@ -1,5 +1,5 @@
 import { DialogBackdrop, DialogContent, DialogPositioner } from "@ark-ui/solid";
-import { Accessor, Component, JSX, Show } from "solid-js";
+import { Accessor, Component, JSX, Show, children, untrack } from "solid-js";
 import { Portal } from "solid-js/web";
 import { Transition } from "solid-transition-group";
 
@@ -26,59 +26,71 @@ const dialogAnim = () => [
 
 export const Modal: Component<ModalProps> = (props) => {
   return (
-    <Portal>
-      <Transition
-        onEnter={(el, done) => {
-          const a = el.animate(backdropAnim(), {
-            duration: 150,
-          });
-          a.finished.then(done);
-        }}
-        onExit={(el, done) => {
-          const a = el.animate(backdropAnim().reverse(), {
-            duration: 150,
-          });
-          a.finished.then(done);
-        }}
-      >
-        <Show when={props.isOpen()}>
-          {/* Max zIndex ends in 47, Modal ends in 46 to display Toasts in front */}
-          <DialogBackdrop
-            class="fixed inset-0 bg-black/50 backdrop-blur-sm backdrop-filter"
-            style={{
-              "z-index": 2147483646,
-            }}
-          />
-        </Show>
-      </Transition>
+    <>
+      {untrack(() => {
+        const content = children(() => (
+          <>
+            <Transition
+              onEnter={(el, done) => {
+                const a = el.animate(backdropAnim(), {
+                  duration: 150,
+                });
+                a.finished.then(done);
+              }}
+              onExit={(el, done) => {
+                const a = el.animate(backdropAnim().reverse(), {
+                  duration: 150,
+                });
+                a.finished.then(done);
+              }}
+            >
+              <Show when={props.isOpen()}>
+                {/* Max zIndex ends in 47, Modal ends in 46 to display Toasts in front */}
+                <DialogBackdrop
+                  class="fixed inset-0 bg-black/50 backdrop-blur-sm backdrop-filter"
+                  style={{
+                    "z-index": 2147483646,
+                  }}
+                />
+              </Show>
+            </Transition>
 
-      <Transition
-        onEnter={(el, done) => {
-          const a = el.animate(dialogAnim(), {
-            duration: 250,
-          });
-          a.finished.then(done);
-        }}
-        onExit={(el, done) => {
-          const a = el.animate(dialogAnim().reverse(), {
-            duration: 150,
-          });
-          a.finished.then(done);
-        }}
-      >
-        <Show when={props.isOpen()}>
-          <DialogPositioner
-            class="fixed inset-0 flex h-full w-full items-center justify-center"
-            style={{
-              "z-index": 2147483646,
-            }}
-          >
-            <DialogContent class="mx-2 flex w-96 flex-col rounded-3xl border border-stone-200 border-opacity-10 bg-stone-900 bg-opacity-90 p-6 text-white backdrop-blur-xl backdrop-filter">
-              {props.children}
-            </DialogContent>
-          </DialogPositioner>
-        </Show>
-      </Transition>
-    </Portal>
+            <Transition
+              onEnter={(el, done) => {
+                const a = el.animate(dialogAnim(), {
+                  duration: 250,
+                });
+                a.finished.then(done);
+              }}
+              onExit={(el, done) => {
+                const a = el.animate(dialogAnim().reverse(), {
+                  duration: 150,
+                });
+                a.finished.then(done);
+              }}
+            >
+              <Show when={props.isOpen()}>
+                <DialogPositioner
+                  class="fixed inset-0 flex h-full w-full items-center justify-center"
+                  style={{
+                    "z-index": 2147483646,
+                  }}
+                >
+                  <DialogContent class="mx-2 flex w-96 flex-col rounded-3xl border border-stone-200 border-opacity-10 bg-stone-900 bg-opacity-90 p-6 text-white backdrop-blur-xl backdrop-filter">
+                    {props.children}
+                  </DialogContent>
+                </DialogPositioner>
+              </Show>
+            </Transition>
+          </>
+        ));
+
+        return (
+          <Show when={content.toArray().length}>
+            <Portal>{content()}</Portal>
+          </Show>
+        );
+      })}
+    </>
   );
 };
