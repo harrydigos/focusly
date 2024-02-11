623ecd1d72496e541ebe9bb3ee995f9e21df0f65
Harry Digos <harrydigos@gmail.com>
2024-02-11 14:38:25 +0200

    feat: add tooltip to menu buttons

--- src/design/Tooltip/index.tsx
+++ src/design/Tooltip/index.tsx
@@ -1,12 +1,13 @@
 import { Tooltip as KobalteTooltip } from "@kobalte/core";
-import { Component, JSX, splitProps } from "solid-js";
+import { Component, JSX, Show, splitProps } from "solid-js";
 import { TooltipRootProps } from "@kobalte/core/dist/types/tooltip";
 import styles from "./Tooltip.module.css";
 
 export type TooltipProps = {
   children: JSX.Element;
-  description: string;
+  description: string | JSX.Element;
   arrowSize?: number;
+  animate?: boolean;
 } & TooltipRootProps;
 
 export const Tooltip: Component<TooltipProps> = (props) => {
@@ -14,6 +15,7 @@ export const Tooltip: Component<TooltipProps> = (props) => {
     "children",
     "description",
     "arrowSize",
+    "animate",
   ]);
 
   return (
@@ -21,13 +23,21 @@ export const Tooltip: Component<TooltipProps> = (props) => {
       <KobalteTooltip.Trigger>{local.children}</KobalteTooltip.Trigger>
       <KobalteTooltip.Portal>
         <KobalteTooltip.Content
-          class={`${styles.tooltip__animation} animate-fadeIn rounded-lg border border-stone-700 bg-stone-900 px-2 py-1.5 text-xs font-normal text-stone-50 drop-shadow-md`}
+          class="rounded-lg border border-stone-700 bg-stone-900 px-2 py-1.5 text-xs font-normal text-stone-50 drop-shadow-md"
+          classList={{
+            [styles.tooltip__animation]: local.animate ?? true,
+          }}
           style={{
             "z-index": 2147483647,
           }}
         >
           <KobalteTooltip.Arrow size={local.arrowSize || 0} />
-          <p>{local.description}</p>
+          <Show
+            when={typeof local.description !== "string"}
+            fallback={<p>{local.description}</p>}
+          >
+            {local.description}
+          </Show>
         </KobalteTooltip.Content>
       </KobalteTooltip.Portal>
     </KobalteTooltip.Root>
